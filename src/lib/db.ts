27352f706261c8e25cd3543f654ca64f1dd7f304
Alyxia Sother <alyxia@riseup.net>
2024-02-26 10:39:52 +0100

    [pages/*] Added ingredient editing page/endpoint

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 export const db = new PrismaClient();
+
+// Types {{{
+// see: <https://www.prisma.io/docs/orm/prisma-client/type-safety/operating-against-partial-structures-of-model-types>
+const ingredientWithInfo = Prisma.validator<Prisma.IngredientDefaultArgs>()({
+  include: { ingredientInfo: true },
+});
+
+export type IngredientWithInfo = Prisma.IngredientGetPayload<typeof ingredientWithInfo>;
+// }}}
---
+++ src/pages/api/ingredients/edit.ts
@@ -0,0 +1,36 @@
+import type { APIRoute } from 'astro';
+import { db } from '../../../lib/db';
+
+// TODO: GENERALISE!!! Use the central-endpoint-with-operation-handler paradigm
+
+export const POST: APIRoute = async (context) => {
+  const formData = await context.request.formData();
+  const data = Object.fromEntries(
+    Array.from(formData.keys()).map((key) => [
+      key,
+      formData.getAll(key).length > 1 ? formData.getAll(key) : formData.get(key),
+    ]),
+  );
+
+  await db.ingredient.update({
+    where: {
+      id: Number(data.id),
+    },
+    data: {
+      amount: Number(data.amount),
+      archived: false,
+      expirationDate: new Date(String(data.expr_date)),
+      ingredientInfo: {
+        update: {
+          name: String(data.name),
+          calories: Number(data.calories),
+          unit: String(data.unit),
+        },
+      },
+    },
+  });
+
+  return new Response('Success', {
+    status: 201,
+  });
+};
