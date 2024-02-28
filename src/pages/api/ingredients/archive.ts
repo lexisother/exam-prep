ca4da7f2558a8a730f91313045b7c13667efccfa
Alyxia Sother <alyxia@riseup.net>
2024-02-28 13:25:29 +0100

    [K/pages/ingredients] Implement archiving

---
+++ src/pages/api/ingredients/archive.ts
@@ -0,0 +1,41 @@
+import type { APIRoute } from 'astro';
+import { json } from '../../../lib/util';
+import { db } from '../../../lib/db';
+
+interface Data {
+  id: string;
+  archived: string;
+}
+
+export const POST: APIRoute = async (context) => {
+  const res = await json<Data>(context.request);
+
+  console.log(res);
+
+  try {
+    await db.ingredient.update({
+      where: {
+        id: Number(res.id),
+      },
+      data: {
+        // FUUUUUUUUUCK
+        // eslint-disable-next-line no-unneeded-ternary
+        archived: res.archived === undefined ? true : false,
+      },
+    });
+  } catch (e) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        error: e,
+      }),
+    );
+  }
+
+  return new Response(
+    JSON.stringify({
+      success: true,
+      error: null,
+    }),
+  );
+};
